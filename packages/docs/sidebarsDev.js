2486037ffffc960c99c14cd77a1945810c4bf82b
RMO <rmo@black.com>
2025-08-26 23:57:36 -0600

    docs: add box forge documentation and tests

--- packages/docs/sidebarsDev.js
+++ packages/docs/sidebarsDev.js
@@ -7,5 +7,15 @@ module.exports = {
     { type: "doc", id: "security-privacy" },
     { type: "doc", id: "browser-support" },
     { type: "doc", id: "licensing" },
+    {
+      type: "category",
+      label: "Box Forge",
+      items: [
+        { type: "doc", id: "box-forge/overview" },
+        { type: "doc", id: "box-forge/schema-format" },
+        { type: "doc", id: "box-forge/examples" },
+        { type: "doc", id: "box-forge/integration" },
+      ],
+    },
   ],
 };
